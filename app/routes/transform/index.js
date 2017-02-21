9ae76f698b2e1a6d20c6a137413416ade99e7fa8
leiming8 <leiming8@wanda.cn>
2017-02-21 15:21:49 +0800

    Add Replace

---
+++ app/routes/transform/index.js
@@ -0,0 +1,70 @@
+import Router from 'koa-router'
+import fse from 'fs-extra'
+import path from 'path'
+import process from 'process'
+import glob from 'glob'
+
+
+const injectJavaScriptIntoHTML = html => {
+  const headRegExp = /(<\/head>)/i;
+  if (headRegExp.test(html)) {
+    return html.replace(headRegExp, matched => {
+      return '<script> var obj = 123</script>\n' + matched
+    })
+  } else {
+    // TODO: NOT <head/>
+    return html
+  }
+}
+
+/**
+ * @param template
+ * @return promise
+ */
+const getTemplateNames = template => new Promise((resolve, reject) => {
+  glob('*.html', {
+    cwd: path.resolve(process.cwd(), 'packages', template)
+  }, (err, files) => {
+    if (err) {
+      return reject(err)
+    } else {
+      return resolve(files)
+    }
+  })
+})
+
+export const transform = app => {
+  const router = new Router({
+    prefix: '/transform'
+  })
+
+  router.get('/:template',
+    async(ctx, next) => {
+      const { template } = ctx.params
+
+      const TemplateNames = await getTemplateNames(template)
+      if (!TemplateNames.length) {
+        return ctx.body = `The template of ${template} could not find any html files.`
+      } else {
+        TemplateNames.map(templateName => {
+          const templatePath = path.resolve(process.cwd(), 'packages', template, templateName)
+          const oldHTML = fse.readFileSync(templatePath, "utf-8")
+          const newHTML = injectJavaScriptIntoHTML(oldHTML)
+          fse.ensureDirSync(path.resolve(process.cwd(), 'static', template))
+          fse.writeFileSync(path.resolve(process.cwd(), 'static', template, templateName), newHTML, { encoding: 'utf-8' })
+        })
+
+        ctx.body = TemplateNames
+      }
+    })
+
+  router.get('/',
+    async(ctx, next) => {
+      ctx.body = '/transform/:name must be exist'
+    })
+
+  app.use(router.routes())
+  app.use(router.allowedMethods())
+}
+
+export default transform
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import Koa from 'koa'
 const app = new Koa()
 const port = 7777;
+import transform from './app/routes/transform'
+transform(app)
 
-app.use(async function (ctx, next) {
-  console.log('>> one');
-  await next();
-  console.log('<< one');
-});
-
-app.listen(port)
+if (!module.parent) app.listen(port)
 console.log(`\nhttp://localhost:${port}/\n`)
